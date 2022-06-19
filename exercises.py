760b72af127ce8261468ab5c7125099582ef1ded
Akafack Jauvany <jauvany@yahoo.com>
2022-06-19 15:55:23 +0300

    Update exercises.py
    6 Added

--- exercises.py
+++ exercises.py
@@ -1,3 +1,4 @@
+#01
 Exponentiation
 
 
@@ -6,4 +7,70 @@ Output the result using the print() statement.
 
 #your code goes here
 print(4**5)
+
+Flight Time
+
+#02
+You need to calculate the flight time of an upcoming trip. You are flying from LA to Sydney, covering a distance of 7425 miles, the plane flies at an average speed of 550 miles an hour.
+
+Calculate and output the total flight time in hours.
+
+Hint
+The result should be a float.
+Use the print statement to output the result.
+
+"""Solution """
+print(7425 / 550)
+
+
+#03
+Strings
+
+
+You are given a code that should output a string with quotes.
+However, it contains errors.
+print('I'm learning Python. It's easy.')
+
+Task
+Fix the given code to generate the expected output.
+Remember, you need to escape the quotes in the strings to fix the errors in the code.
+
+"""Solution"""
+
+print('I\'m learning Python. It\'s easy.')
+
+#04
+Strings
+
+
+Write a program to output the letters A B C D, each on a separate line.
+You can use 3 quotes to add the new lines.
+
+"""solution"""
+
+print('''A
+B
+C
+D''')
+
+#05
+String Operations
+
+
+You’re given a task to write the word "hi" 42 times. Boring, right?
+
+Write a program to do it for you.
+
+Create a program to output "hi" 42 times, without any separators, on the same line.
+Remember, you can multiply strings by numbers.
+
+"""Solution"""
+#your code goes here
+print("hi" * 42)
+
+#06
+
+
+
+
 

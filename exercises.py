7302643373b2386a126d53388d2866b10f6099ee
Akafack Jauvany <jauvany@yahoo.com>
2022-06-28 12:04:55 +0300

    Update exercises.py
    #10 and more

--- exercises.py
+++ exercises.py
@@ -139,7 +139,31 @@ print(bill*20/100)
 
 #10
 
+Problem
 
+Write a program that checks if the water is boiling.
+
+if Statements
+
+
+Write a program that checks if the water is boiling.
+Take the integer temperature in Celsius as input and output "Boiling" if the temperature is above or equal to 100.
+
+Sample Input
+105
+
+Sample Output
+Boiling
+Do not output anything if the water is not boiling.
+
+"""Solution"""
+
+temp = int(input())
+if temp >= 100:
+    print("Boiling")
+    
+#11 
+    
 
 
 

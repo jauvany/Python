e816a7fca4a2098e74f80a0475ef078e4609ea57
Akafack Jauvany <jauvany@yahoo.com>
2022-06-30 09:04:59 +0300

    Update exercises.py
    #13

--- exercises.py
+++ exercises.py
@@ -244,6 +244,26 @@ elif age > 18 and age < 65:
     
 #13
 
+Problem
+while Loops
+
+
+You are given a program that outputs all the numbers from 0 to 10.
+Change the code to make it output only the even numbers.
+Any integer that can be divided exactly by 2 is an even number.
+
+""" SOLUTION """
+
+x = 0
+while x<=10:
+  if x%2 == 0:
+   print(x)
+  x+=1
+    
+#14
+
+
+
 
 
 

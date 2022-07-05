632bb46579f5e68b5126dfc59e597760095967c6
Akafack Jauvany <jauvany@yahoo.com>
2022-07-05 17:43:46 +0300

    Update exercises.py
    #14

--- exercises.py
+++ exercises.py
@@ -261,6 +261,29 @@ while x<=10:
   x+=1
     
 #14
+Problem
+continue
+
+
+You are making a ticketing system.
+The price of a single ticket is $100.
+For children under 3 years of age, the ticket is free.
+
+Your program needs to take the ages of 5 passengers as input and output the total price for their tickets.
+
+Sample Input
+18
+24
+2
+5
+42
+
+Sample Output
+400
+There is one child under 3 among the passengers, so the total price of 5 tickets is $400.
+
+""" SOLUTION """
+
 
 
 

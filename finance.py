3066369fa63c69bb58f0918baca5a01e59f00406
Akafack Jauvany <jauvany@yahoo.com>
2022-07-11 01:29:20 +0300

    Create finance.py
    #01

---
+++ finance.py
@@ -0,0 +1,20 @@
+#01
+Problem
+
+Inflation
+
+We need to calculate how much an item worth $8000 today will cost after 10 years, if the annual inflation rate is 5%.
+
+Hint: Use the following formula to calculate the future value: present value*(1+inflation rate)**years
+Note that the inflation rate should be expressed as a decimal, so 5% corresponds to the value 0.05,
+
+Write a program to calculate and output the result.
+Use the formula in the print statement with the given values.
+
+""" SOLUTION """
+
+present_value = 8000
+annual_inflation = 1 + 0.05
+print(present_value * (annual_inflation) ** 10)
+
+#02

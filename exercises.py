4abfd0d764f60b52e1b9577de253037e476380f1
Akafack Jauvany <jauvany@yahoo.com>
2022-07-19 23:08:04 +0300

    Update exercises.py
    #21

--- exercises.py
+++ exercises.py
@@ -313,7 +313,31 @@ print(sum(x))
 
 OR
 
+#21
+Problem
 
+Ranges
+
+
+You are creating a date picker for a website and need to output all of the years in a given period.
+Write a program that takes two integers as input and outputs the range of numbers between the two inputs as a list.
+The output sequence should start with the first input number and end with the second input number, without including it.
+
+Sample Input
+2005
+2011
+
+Sample Output
+[2005, 2006, 2007, 2008, 2009, 2010]
+Convert a range object to a list and output it.
+
+""" Solution """
+
+a = int(input())
+b = int(input())
+
+list = list(range(a,b))
+print(list)
 
 
 

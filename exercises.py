4e1e75484eb92175ac240f4220d61113c1347474
Akafack Jauvany <jauvany@yahoo.com>
2022-07-23 21:19:02 +0300

    Update exercises.py
    #18

--- exercises.py
+++ exercises.py
@@ -355,6 +355,32 @@ print(msg)
 
 
 #18
+Functions
+
+
+We have a function that outputs "Welcome, user" as it is called. We want to make it more personalized, so redesign the given function so that it will take the name of the user as input and output the welcome message with it.
+
+Sample Input
+Tommy
+
+Sample Output
+Welcome, Tommy
+Don't forget to indent the statement in the function.
+         
+""" SOLUTION """
+         
+def welcome():
+   print("Welcome, " + input())
+
+welcome()
+         
+OR
+         
+def welcome():
+   print( "Welcome, + name)
+
+name = input()
+welcome( )         
 
 #19
 
